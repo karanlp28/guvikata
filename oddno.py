a012ed9785dc8e9dae7e9feb315dcfd000f9dacb
karanlp28 <46442151+karanlp28@users.noreply.github.com>
2019-01-08 15:42:47 +0530

    Update and rename prime.py to oddno.py

---
+++ oddno.py
@@ -0,0 +1,5 @@
+n,q=map(int, input().split())
+for i in range(n,q+1):
+    if i%2==1:
+        print(i,end=" ")
+    
--- prime.py
+++
@@ -1,5 +0,0 @@
-n=int(input())
-if n%2==1 or n%2==0:
-    print("yes")
-else:
-    print("no")
